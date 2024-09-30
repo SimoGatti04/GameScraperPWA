01733ec96711899bf180ad29589ce36a26e73448
SimoGatti04 <55622211+SimoGatti04@users.noreply.github.com>
2024-09-30 12:28:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ let apiBaseUrl = '';
 if (isTestMode) {
     apiBaseUrl = 'http://localhost:8080';
 } else {
-    apiBaseUrl = 'https://curly-numbers-throw.loca.lt';
+    apiBaseUrl = 'gamescraper.simogatti.site';
 }
 
 export const config = {
